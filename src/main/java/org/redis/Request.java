665272fe9cc307bdcf539c3e51ecf66857d35adf
Shubham Upadhyay <01shubhamupadhyay@gmail.com>
2025-08-04 21:44:41 +0530

    chore: clean up code in request

--- src/main/java/org/redis/Request.java
+++ src/main/java/org/redis/Request.java
@@ -2,32 +2,50 @@ package org.redis;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class Request {
-    private final String type;
-    private final String key;
-    private final String value
+    private String type;
+    private String key;
+    private String value;
     private final RequestValidationCode requestValidationCode;
 
-    public Request(List<String> input){
+    public Request(String type, String key) {
+        this.type = type;
+        this.key = key;
+        this.requestValidationCode = RequestValidationCode.VALID;
+    }
+
+    public Request(String type, String key, String value){
+        this.type = type;
+        this.key = key;
+        this.value = value;
+        this.requestValidationCode = RequestValidationCode.VALID;
+    }
+
+    public Request(RequestValidationCode requestValidationCode){
+        this.requestValidationCode = requestValidationCode;
+    }
+
+
+    public static Optional<Request> fromInput(List<String> input){
         if ((input.size() > 3 || input.size() < 2)
-                || (Objects.equals(input.getFirst(),"set") && input.size() != 2)
-                || (Objects.equals(input.getFirst(),"get") && input.size() != 3) ){
-            requestValidationCode = RequestValidationCode.INVALID_NUMBER_OF_PARAMETERS;
+                || (Objects.equals(input.getFirst(),"set") && input.size() != 3)
+                || (Objects.equals(input.getFirst(),"get") && input.size() != 2) ){
+            return Optional.of(new Request(RequestValidationCode.INVALID_NUMBER_OF_PARAMETERS));
         }
-        else if(!Objects.equals(input.getFirst().toLowerCase(),"set")
-                || !Objects.equals(input.getFirst().toLowerCase(),"get")){
-            requestValidationCode = RequestValidationCode.INVALID_METHOD;
-        }
-        else requestValidationCode = RequestValidationCode.VALID;
-
-        if (requestValidationCode != RequestValidationCode.VALID){
-            throw new RuntimeException("Unsupported Method or Arguments");
+        else if(!(Objects.equals(input.getFirst().toLowerCase(),"set")
+                || Objects.equals(input.getFirst().toLowerCase(),"get"))){
+            return Optional.of(new Request(RequestValidationCode.INVALID_METHOD));
         }
 
-        this.type = input.getFirst().toLowerCase();
-        this.key = input.get(1);
-        this.value = (input.size() == 3) ? input.getLast() : null;
+
+        if (input.getFirst().equalsIgnoreCase("set")) {
+            return Optional.of(new Request(input.getFirst(), input.get(1), input.getLast()));
+        }
+        else{
+            return Optional.of(new Request(input.getFirst(), input.get(1)));
+        }
     }
 
     public String getType() {
@@ -44,5 +62,13 @@ public class Request {
 
     public boolean isGet() {
         return Objects.equals(type,"get");
+    }
+
+    public boolean isValid(){
+        return requestValidationCode == RequestValidationCode.VALID;
+    }
+
+    public RequestValidationCode getRequestValidationCode(){
+        return requestValidationCode;
     }
 }
